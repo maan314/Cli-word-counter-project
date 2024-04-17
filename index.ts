14cbb9067ccff18d85de0a80569a34f9802e641e
muhammad usman <usmanqaisrani110@gmail.com>
2024-04-17 19:46:35 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+const answers: {
+    Sentence :string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: chalk.red("Enter you sentence to count the word:")
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+console.log(words);
+console.log(chalk.blue(`Your Sentence word count is: ${words.length}`));
